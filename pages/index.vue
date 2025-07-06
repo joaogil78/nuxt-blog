234aa23c514ccb070c8da9fcdea3f70ce0cb0897
joaogil78 <joaoreck@yahoo.com.br>
2025-07-06 03:00:43 -0300

    Firs commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-1/2 mx-auto my-10">
+    <h1 class="text-5xl font-semibold text-violet-700">Nuxt Blog</h1>
+
+    <article v-for="post in posts" :key="post.id" class="my-7 pb-3">
+      <h2 class="text-3xl text-violet-750 mb-3">{{ post.title }}</h2>      
+      <p class="mb-3">{{ post.body }}</p>
+      <NuxtLink :to="{ name:'post-id', params: { id: post.id }}" class="inline-block border px-5 py-3 rounded text-violet-700 font-bold">Leia +</NuxtLink>
+    </article>
+  
+  </div>
+</template>
+
+<script setup>
+import { NuxtLink } from '#components';
+import { ref } from 'vue'
+
+const posts = ref([])
+
+fetch('https://dummyjson.com/posts/')
+  .then(response => response.json())
+  .then(json => {
+    console.log(json.posts);
+    posts.value = json.posts;
+  });
+</script>
+
+<style scoped>
+article { border-bottom:1px solid #8200db; }
+</style>
---
+++ pages/post/[id].vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="w-1/2 mx-auto my-10">
+        <h2 class="text-2xl font-semibold text-violet-700">Nuxt Blog</h2>
+
+        <article class="my-7 pb-3">
+        <h1 class="text-6xl text-violet-750 mb-4">{{ post.title }}</h1>      
+        <p class="mb-3">{{ post.body }}</p>
+        </article>  
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const route = useRoute()
+const post = ref('') 
+
+onMounted(() => {
+    fetch('https://dummyjson.com/posts/'+route.params.id)
+        .then(response => response.json())
+        .then(json => post.value = json)
+})
+</script>
+
+<style scoped>
+h1::first-letter { text-transform: capitalize; }
+</style>
